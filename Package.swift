688836505cf50e63a7bc16611438330d4aeba1fa
Lismond Bernard <lismond@acst.com>
2020-01-03 12:23:57 -0500

    Added Swift Package Manager capability

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "KVOController",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "KVOController",
+            targets: ["KVOController"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "KVOController",
+            dependencies: []),
+    ]
+)
