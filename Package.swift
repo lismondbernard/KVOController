703711561feaa19b0914803d16d8e83e4f092361
Lismond Bernard <lismond@acst.com>
2020-01-08 16:10:53 -0500

    added dynamic

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "KVOController",
+	    type: .dynamic,
             targets: ["KVOController"]),
     ],
     dependencies: [
