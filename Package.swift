31cf95f68568388a55ccef4e55adddf41002d0d4
Lismond Bernard <lismond@acst.com>
2020-01-09 15:55:15 -0500

    added Package.swift to v1.1.0

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "KVOController",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "KVOController",
+	    type: .dynamic,
+            targets: ["KVOController"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "KVOController",
+            dependencies: [],
+	    path: "FBKVOController",
+	    publicHeadersPath: "FBKVOController"),
+    ]
+)
